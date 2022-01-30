a410fec4e4fb50f2b3341e2eb13e4ec37506ea2b
= <maria.louisa.castro@gmail.com>
2022-01-30 22:14:24 +0000

    Begin JS for stopwatch

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,22 +1,64 @@
-import "./Stopwatch.css";
+import React, { useState, useEffect } from "react";
 import start from "./images/play.png";
 import stop from "./images/pause.png";
-import cancel from "./images/cancel.png";
+import reset from "./images/cancel.png";
+import "./Stopwatch.css";
 
 export default function Stopwatch(props) {
+  const [timer, setTimer] = useState("00:00:00");
+
+  let [milliseconds, seconds, minutes] = [0, 0, 0];
+  let int = 0;
+
+  function startTimer() {
+    int = setInterval(() => showTime(), 10);
+  }
+
+  function stopTimer() {
+    console.log("stop");
+    clearInterval(int);
+  }
+
+  function resetTimer() {
+    console.log("reset");
+    clearInterval(int);
+    [milliseconds, seconds, minutes] = [0, 0, 0];
+    setTimer("00:00:00");
+  }
+
+  function showTime() {
+    milliseconds += 10;
+
+    if (milliseconds == 1000) {
+      milliseconds = 0;
+      seconds++;
+
+      if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+      }
+    }
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = milliseconds / 10;
+    if (ms < 10) ms = ms + "0";
+
+    setTimer(`${m}:${s}:${ms}`);
+  }
+
   if (props.active) {
     return (
       <div className="Stopwatch popup">
         <p>STOPWATCH</p>
-        <p className="timer">00:00:00</p>
-        <button onclick="start()" className="play">
+        <p className="timer">{timer}</p>
+        <button onClick={startTimer} className="play">
           <img src={start} />
         </button>
-        <button onclick="stop()" className="stop">
+        <button onClick={stopTimer} className="stop">
           <img src={stop} />
         </button>
-        <button onclick="reset()" className="reset">
-          <img src={cancel} />
+        <button onClick={resetTimer} className="reset">
+          <img src={reset} />
         </button>
       </div>
     );
