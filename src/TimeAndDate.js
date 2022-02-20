ae78665eaed459098b9d23bdd444fc7e1d80be13
= <maria.louisa.castro@gmail.com>
2022-02-20 22:44:21 +0000

    Add minor styling to Date component

--- src/TimeAndDate.js
+++ src/TimeAndDate.js
@@ -6,25 +6,29 @@ import "./Home.css";
 export default function TimeAndDate(props) {
   const [dateState, setDateState] = useState(new Date());
   useEffect(() => {
-    setInterval(() => setDateState(new Date()), 30000);
+    setInterval(() => setDateState(new Date()), 1000);
   }, []);
   if (props.active) {
     return (
       <div className="Date popup">
         <p className="TodayDate">
-          {" "}
           {dateState.toLocaleDateString("en-GB", {
             weekday: "long",
             day: "numeric",
+          })}
+          <span>th</span>
+        </p>
+        <p className="TodayDate2">
+          {dateState.toLocaleDateString("en-GB", {
             month: "long",
-            year: "numeric",
+            year: "2-digit",
           })}
         </p>
         <p className="TodayTime">
           {dateState.toLocaleString("en-GB", {
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
             hour12: false,
           })}
         </p>
